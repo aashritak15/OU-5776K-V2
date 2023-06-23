317f0fcc80b56be3cbe2ed0f7e7ab203670371ca
Aashrita Koyyalamudi <aashrita.koyyalamudi@gmail.com>
2023-06-23 15:16:48 -0700

    aashrita

--- include/subsystems/odom.hpp
+++ include/subsystems/odom.hpp
@@ -6,11 +6,12 @@ void encoderAvg();
 void movePID(float target, float maxV);
 void turnPID(float target, bool CW, int ms);
 
-//extern double displacement; 
+/*extern double displacement; 
 
-//void drive(double target);
+void drive(double target);
 
-//bool isMoving();
+bool isMoving();
+*/
 
 void imuInnit();
 void IEInnit(); 
