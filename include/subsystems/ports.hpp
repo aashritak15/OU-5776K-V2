01afbe3df3da9c4278ad114f284024607ba80c45
Aashrita Koyyalamudi <aashrita.koyyalamudi@gmail.com>
2023-07-21 14:56:45 -0700

    shri shri

--- include/subsystems/ports.hpp
+++ include/subsystems/ports.hpp
@@ -17,8 +17,3 @@ const std::uint8_t leftTopPort = 10;
 
 const std::uint8_t imuPort1 = 4; //imu port #1
 const std::uint8_t imuPort2 = 18; //imu port#2
-
-const std::uint8_t pistonPTO1Port = 8; //piston port
-const std::uint8_t intakePort1 = 7; //intake port
-const std::uint8_t intakePort2 = 13;
-
