d0da81121f46504884e726511dfc823eb041e456
Ria <polururia@gmail.com>
2023-07-24 15:56:20 -0700

    Ria + Aashu
    Co-authored-by: aashritak15 <aashritak15@users.noreply.github.com>

--- include/subsystems/path.hpp
+++ include/subsystems/path.hpp
@@ -1,3 +1,86 @@
 #include "main.h"
 
 using namespace okapi;
+
+
+class waypoint {
+private:
+    float x;
+    float y;
+    float theta;
+    float dist;
+    float vel;
+    float curv;
+
+public:
+    waypoint(float x1, float y1){
+        this->x = x1;
+        this->y = y1;
+        this->theta = 0;
+        this->dist = 0;
+        this->vel = 0;
+        this->curv = 0;
+    }
+
+    waypoint(float x1, float y1, float angle){
+        this->x = x1;
+        this->y = y1;
+        this->theta = angle;
+        this->dist = 0;
+        this->vel = 0;
+        this->curv = 0;
+    }
+
+    float getX() const {
+        return this->x
+    }
+
+    float getY() const {
+        return this->y
+    }
+
+    float getAngle() const{
+        return this->theta;
+    }
+
+    float getDist() const {
+        return this->dist;
+    }
+
+    float getVel() const {
+        return this->vel;
+    }
+
+    float getCurv() const {
+        return this->curv;
+    }
+
+    void setX(float newX) {
+        this->x = newX;
+    }
+
+    void setY(float newY) {
+        this->y = newY;
+    }
+
+    void setTheta(float newAngle) {
+        this->theta = newAngle;
+    }
+
+    void setDist(float newDist) {
+        this->dist = newDist;
+    }
+
+    void setVel(float newVel) {
+        this->vel = newVel;
+    }
+
+    void setCurv(float newCurv) {
+        this->curv = newCurv;
+    }
+
+i
+};
+
+
+
--- src/subsystems/path.cpp
+++ src/subsystems/path.cpp
@@ -3,85 +3,48 @@
 #include <math.h>
 #include <cmath>
 
+IntegratedEncoder leftEncoder(leftTopPort, true);
+IntegratedEncoder rightEncoder(rightTopPort, false);
 
-class waypoint {
-private:
-    float x;
-    float y;
-    float theta;
-    float dist;
-    float vel;
-    float curv;
+IMU imu1(imuPort1, IMUAxes::z);
+IMU imu2(imuPort2, IMUAxes::z);
+`
 
-public:
-    waypoint(float x1, float y1){
-        this->x = x1;
-        this->y = y1;
-        this->theta = 0;
-        this->dist = 0;
-        this->vel = 0;
-        this->curv = 0;
+// Bezier curve 
+waypoint calculateBezier( const waypoint& p0, const waypoint& p1, float t){
+    float x = (1 - t) * p0.getX() + t * p1.getX();
+    float y = (1 - t) * p0.getY() + t * p1.getY();
+
+    return waypoint(x, y);
+    
+}
+
+
+
+
+std::vector<waypoint> path(std::vector<waypoint> followPath, float maxVel, float maxA, float curv){
+    // 1. injecting points 
+    int spacing = 6;
+
+    std::vector<waypoint> newPath; 
+
+    for(int i = 0; i < followPath.size() - 1; i++){
+        //points between the waypoint 
+        waypoint start = followPath[i];
+        waypoint end = followPath[i + 1];
+
+        waypoint vector = end_point - start_point;
+
+        int num_points_that_fit = std::ceil(vector.magnitude() / spacing);
+
+        vector = vector.normalize() * spacing;
+
+        for(int j = 0; j < num_points_that_fit; j++){
+            newPath.push_back(start + vector * j)
+        }
     }
+    newPath.push_back(followPath.back());
 
-    waypoint(float x1, float y1, float angle){
-        this->x = x1;
-        this->y = y1;
-        this->theta = angle;
-        this->dist = 0;
-        this->vel = 0;
-        this->curv = 0;
-    }
+    return newPath;
 
-    float getX() const {
-        return this->x
-    }
-
-    float getY() const {
-        return this->y
-    }
-
-    float getAngle() const{
-        return this->theta;
-    }
-
-    float getDist() const {
-        return this->dist;
-    }
-
-    float getVel() const {
-        return this->vel;
-    }
-
-    float getCurv() const {
-        return this->curv;
-    }
-
-    void setX(float newX) {
-        this->x = newX;
-    }
-
-    void setY(float newY) {
-        this->y = newY;
-    }
-
-    void setTheta(float newAngle) {
-        this->theta = newAngle;
-    }
-
-    void setDist(float newDist) {
-        this->dist = newDist;
-    }
-
-    void setVel(float newVel) {
-        this->vel = newVel;
-    }
-
-    void setCurv(float newCurv) {
-        this->curv = newCurv;
-    }
-
-
-};
-
-
-
+}
