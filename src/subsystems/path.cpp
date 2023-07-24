50650338933ff01010f1ed7d7e51f6348bb43aa3
Aashrita Koyyalamudi <aashrita.koyyalamudi@gmail.com>
2023-07-24 16:28:59 -0700

    smoothening (incomplete)

--- src/subsystems/path.cpp
+++ src/subsystems/path.cpp
@@ -8,18 +8,29 @@ IntegratedEncoder rightEncoder(rightTopPort, false);
 
 IMU imu1(imuPort1, IMUAxes::z);
 IMU imu2(imuPort2, IMUAxes::z);
-`
 
-// Bezier curve 
-waypoint calculateBezier( const waypoint& p0, const waypoint& p1, float t){
-    float x = (1 - t) * p0.getX() + t * p1.getX();
-    float y = (1 - t) * p0.getY() + t * p1.getY();
 
-    return waypoint(x, y);
+//quintic spline to smoothen out the curve 
+waypoint quinticSpline(const waypoint& p0, const waypoint& p1, float t){
+    float x0 = p0.getX();
+    float y0 = p0.getY();
     
+    float x1 = p1.getX();
+    float y1 = p1.getY();
+
+    float a = -6 * x0 + 6 * x1;
+    float b = 15 * x0 - 15 * x1;
+    float c = -10 * x0 + 10 * x1;
+    float d = 0;
+    float e = 0;
+    float f = x0;
+
+    float x = a * powf(t, 5) + b * powf(t, 4) + c * powf(t, 3) + d * powf(t, 2) + e * t + f;
+    float y = 0 //needa do the polynomial for this too 
+
+    return waypoint(x,y);
+
 }
-
-
 
 
 std::vector<waypoint> path(std::vector<waypoint> followPath, float maxVel, float maxA, float curv){
