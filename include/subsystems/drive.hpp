2f1b0e8f337808c020e3848f91b6c0de755f276e
dilarac119 <dilara.caglar@icloud.com>
2023-08-07 16:05:52 -0700

    8/7

--- include/subsystems/drive.hpp
+++ include/subsystems/drive.hpp
@@ -11,12 +11,27 @@ extern Motor leftFront;
 extern Motor leftBack;
 extern Motor leftTop;
 
-
-
 extern okapi::MotorGroup left;
 extern okapi::MotorGroup right;
 
+// reverse
+
+extern Motor RVrightFront;
+extern Motor RVrightBack;
+extern Motor RVrightTop;
+
+extern Motor RVleftFront;
+extern Motor RVleftBack;
+extern Motor RVleftTop;
+
+
+extern okapi::MotorGroup RVleft;
+extern okapi::MotorGroup RVright;
+
 
 extern std::shared_ptr<OdomChassisController> drive;
+extern std::shared_ptr<OdomChassisController> RVdrive;
+extern std::shared_ptr<OdomChassisController> driveTemp;
+
 
 void updateDrive();
