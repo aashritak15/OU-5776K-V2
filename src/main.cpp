bda15ff4f575ea9d3f2ca294781898d46dd5703d
Aashrita Koyyalamudi <aashrita.koyyalamudi@gmail.com>
2024-01-08 15:21:01 -0800

    nothing is working

--- src/main.cpp
+++ src/main.cpp
@@ -86,7 +86,15 @@ lemlib::Chassis Chassis(drive, movePID, turnPID, sensors);
 
 */
 
-
+void screen(){
+    while (true) {
+        lemlib::Pose pose = Chassis.getPose(); // get chassis position
+        pros::lcd::print(0, "X: %f", pose.x);
+        pros::lcd::print(1, "Y: %f", pose.y);
+        pros::lcd::print(2, "Theta: %f", pose.theta);
+        pros::delay(10);
+    }
+}
 
 
 
@@ -103,7 +111,15 @@ void initialize() {
 
     
    
-   // Chassis.calibrate();
+     pros::Task chassisCalibrateTask{[=] {
+         Chassis.calibrate();
+    }};
+    
+    Chassis.setPose(0, 0, 0); // X: 0, Y: 0, Heading: 0
+
+	// print odom values to the brain
+    pros::lcd::initialize();
+    pros::Task screen_task(screen);
 
     selector::init();
     //IEInnit();
@@ -163,35 +179,49 @@ void competition_initialize() {
 * with the default priority and stack size whenever the robot is enabled via
 * the Field Management System or the VEX Competition Switch in the autonomous
 * mode. Alternatively, this function may be called in initialize or opcontrol
+
 * for non-competition testing purposes.
 *
 * If the robot is disabled or communications is lost, the autonomous task
 * will be stopped. Re-enabling the robot will restart the task, not re-start it
 * from where it left off.
 */
+
+
+ //ASSET(example_txt); 
+
+
 void autonomous() {
     //turnCounter(120, 1000);
-
-    Chassis.setPose(36,-59,0);
-
-    Chassis.moveToPose(36, -49, 0, 4000/*, {.forwards = true, .chasePower= 18, .minSpeed = 0}*/);
-    //Chassis.moveToPose(20, 15, 90, 4000);
-
-
-
-
-
-   /* 
-   if (side == 0) {
-   updateSkills(2);
-   } else {
-    //updateAuton(side);
-   }
-
-   */
-
-
     
+  
+
+    /*
+ // example movement: Move to x: 20 and y: 15, and face heading 90. Timeout set to 4000 ms
+    Chassis.moveToPose(20, 15, 90, 4000);
+    // example movement: Move to x: 0 and y: 0 and face heading 270, going backwards. Timeout set to 4000ms
+    Chassis.moveToPose(0, 0, 270, 4000, {.forwards = false});
+    // cancel the movement after it has travelled 10 inches
+    Chassis.waitUntil(10);
+    Chassis.cancelMotion();
+    // example movement: Turn to face the point x:45, y:-45. Timeout set to 1000
+    // dont turn faster than 60 (out of a maximum of 127)
+    Chassis.turnTo(45, -45, 1000, true, 60);
+    // example movement: Follow the path in path.txt. Lookahead at 15, Timeout set to 4000
+    // following the path with the back of the robot (forwards = false)
+    // see line 116 to see how to define a path
+    //Chassis.follow(example_txt, 15, 4000, false);
+    // wait until the chassis has travelled 10 inches. Otherwise the code directly after
+    // the movement will run immediately
+    // Unless its another movement, in which case it will wait
+    Chassis.waitUntil(10);
+    pros::lcd::print(4, "Travelled 10 inches during pure pursuit!");
+    // wait until the movement is done
+    Chassis.waitUntilDone();
+    pros::lcd::print(4, "pure pursuit finished!");
+    */
+
+
    
 }
 
